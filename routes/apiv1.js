a96c79f2c1e06895410a6948082f367c5c1fa089
Washington Vizuete Flores <vizueteflores44@hotmail.com>
2019-08-14 00:18:45 +0000

    Cambio de Código sin prober

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -39,7 +39,7 @@ exports.getWeather = function(req, res) {
 };
 router.get('/getWeather', exports.getWeather);
 
-/*
+
 exports.getWeather2 = function(req, res) {
 	var zip = req.query.zip;
 	if( (zip === null) || (typeof(zip) === 'undefined') ) {
@@ -69,6 +69,6 @@ exports.getWeather2 = function(req, res) {
 
 };
 router.get('/getWeather2', exports.getWeather2);
-*/
+
 
 exports.router = router;
